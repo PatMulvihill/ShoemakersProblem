556c6e8269be1c2ec404acb18763137f002505f5
PatMulvihill <mulvihillp993@yahoo.com>
2016-12-14 01:07:32 -0500

    first commit

---
+++ src/ShoemakersProblem.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author Patrick Mulvihill
+ * CMPT 438: Algorithms
+ * Dr. Tian
+ * Project 7
+ * pmulvihill01@manhattan.edu
+ * github.com/PatMulvihill/MaxSubArray
+ */
+public class ShoemakersProblem {
+    
+    public static void main(String[] args) {
+        
+    }
+}
